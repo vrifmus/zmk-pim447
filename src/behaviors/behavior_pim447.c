a98d283662827c1d55d100549f319013191292d8
ezthrow <vrifmus@gmail.com>
2025-10-15 09:29:04 +0300

    Update behavior_pim447.c

--- src/behaviors/behavior_pim447.c
+++ src/behaviors/behavior_pim447.c
@@ -184,7 +184,7 @@ BEHAVIOR_DT_INST_DEFINE(
     NULL,                                // PM control function
     NULL,                                // Data pointer
     NULL,                                // Configuration pointer
-    POST_KERNEL,                         // Initialization level
+    APPLICATION,                         // Initialization level
     CONFIG_KERNEL_INIT_PRIORITY_DEFAULT, // Initialization priority
     &behavior_pim447_driver_api          // Driver API pointer
 );
